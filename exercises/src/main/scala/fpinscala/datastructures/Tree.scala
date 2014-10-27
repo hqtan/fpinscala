670f69f1a910a2396f4095fa90218806943cd140
haqitan <hanqing.tan@gmail.com>
2014-10-27 23:55:18 +1100

    ex 3.25 - exercises with Tree ADTs

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,21 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  //my implementation works, but soo many cases to consider...
+  def size2[A](b: Tree[A]): Int =
+    b match {
+      case Branch(l:Branch[A], r:Branch[A]) => 1 + size2(l) + size2(r)
+      case Branch(l:Leaf[A], r:Leaf[A]) => 3
+      case Branch(l:Branch[A], r:Leaf[A]) => 2 + size2(l)
+      case Branch(l:Leaf[A], r:Branch[A]) => 2 + size2(r)
+      case Leaf(_) => 1
+    }
 
-
-
-
+  //author's implementation is much simpler; simpler is nicer IMHO
+  def size[A](b: Tree[A]): Int =
+    b match {
+      case Leaf(_) => 1
+      case Branch(l, r) => 1 + size(l) + size(r)
+    }
+  
 }
