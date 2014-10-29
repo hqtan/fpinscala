eff7664ddbf22448761eba90262f454b03f8f136
hqtan <han-qing.tan@rea-group.com>
2014-10-29 11:25:50 +1100

    ex3.26

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,6 +6,7 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  //ex3.25
   //my implementation works, but soo many cases to consider...
   def size2[A](b: Tree[A]): Int =
     b match {
@@ -23,4 +24,13 @@ object Tree {
       case Branch(l, r) => 1 + size(l) + size(r)
     }
   
+  //3.26
+  def maximum(t: Tree[Int]): Int = {
+    def go(t: Tree[Int], z: Int): Int =
+      t match {
+        case Leaf(x) => x max z 
+        case Branch(l, r) => go(l, z) max go(r, z)
+      }
+    go(t, -1)
+  }
 }
