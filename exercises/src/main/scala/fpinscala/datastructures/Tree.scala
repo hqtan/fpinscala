e8ebcc23b5e9f30a187e366f04309e3ef3590d81
haqitan <hanqing.tan@gmail.com>
2014-10-31 23:54:39 +1100

    ex3.27; removed crappy implementation of size()

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,16 +7,6 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
   //ex3.25
-  //my implementation works, but soo many cases to consider...
-  def size2[A](b: Tree[A]): Int =
-    b match {
-      case Branch(l:Branch[A], r:Branch[A]) => 1 + size2(l) + size2(r)
-      case Branch(l:Leaf[A], r:Leaf[A]) => 3
-      case Branch(l:Branch[A], r:Leaf[A]) => 2 + size2(l)
-      case Branch(l:Leaf[A], r:Branch[A]) => 2 + size2(r)
-      case Leaf(_) => 1
-    }
-
   //author's implementation is much simpler; simpler is nicer IMHO
   def size[A](b: Tree[A]): Int =
     b match {
@@ -25,12 +15,47 @@ object Tree {
     }
   
   //3.26
-  def maximum(t: Tree[Int]): Int = {
+  def maximum2(t: Tree[Int]): Int = {
     def go(t: Tree[Int], z: Int): Int =
       t match {
         case Leaf(x) => x max z
         case Branch(l, r) => go(l, z) max go(r, z)
       }
     go(t, -1)
+  }
+  
+  //simpler implementation of maximum func()
+  def maximum(t: Tree[Int]): Int = 
+    t match {
+      case Leaf(x) => x
+      case Branch(l, r) => maximum(l) max maximum(r)
+    }
+
+  //3.27
+  /*
+  def depth[A](t: Tree[A], n: A): Int = {
+    def go[A](t: Tree[A], z: Int): Int =
+      t match {
+        case Leaf(x) =>  if (x == n) z else 0
+        case Branch(l, r) => go(l, z+1) + go(r, z+1)
+      }
+    go(t, 0)
+  }
+  */
+ 
+  def depth[A](t: Tree[A]): Int = 
+    t match {
+        case Leaf(_) => 0
+        case Branch(l, r) => (1 + depth(l)) max (1 + depth(r))
+    }
+
+  //@annotation.tailrec
+  def depth2[A](t: Tree[A]): Int = {
+    def go[A](t: Tree[A], z: Int): Int =
+      t match {
+          case Leaf(_) => z
+          case Branch(l, r) => go(l, z+1) max go(r, z+1)
+      }
+      go(t, 0)
   }
 }
